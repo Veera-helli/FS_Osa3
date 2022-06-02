1a3110212af80f9ea5ca3458901bba90080ed7da
Veera Ihalainen <Veera.helli@gmail.com>
2022-06-02 22:33:53 +0300

    3.6 checked and updated

--- index.js
+++ index.js
@@ -1,96 +1,95 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let persons = [
-    { 
-        id: 1,
-        name: 'Arto Hellas', 
-        number: '040-123456' 
-    },
-    { 
-        id: 2,
-        name: 'Ada Lovelace', 
-        number: '39-44-5323523' 
-    },
-    { 
-        id: 3,
-        name: 'Dan Abramov', 
-        number: '12-43-234345' 
-    },
-    { 
-        id: 4,
-        name: 'Mary Poppendieck', 
-        number: '39-23-6423122' 
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/info', (req, res) => {
-    message = `Phonebook has info for ${persons.length} people.  ${new Date()}`
-    res.send(message)
-})
+app.get("/info", (req, res) => {
+  message = `Phonebook has info for ${persons.length} people.  ${new Date()}`;
+  res.send(message);
+});
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
-})
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {    
-        response.json(person)  
-    } else {    
-        response.status(404).end()  
-    }
-})
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-})
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
 
 const generateId = () => {
-    min = Math.ceil(5);
-    max = Math.floor(200);
-    return Math.floor(Math.random() * (max - min) + min);
-}
+  min = Math.ceil(5);
+  max = Math.floor(200);
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
-    if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
-    }
-    if (!body.number) {
-        return response.status(400).json({ 
-          error: 'number missing' 
-        })
-    }
-    if (persons.map(p => p.name).includes(body.name)) {
-        return response.status(400).json({ 
-          error: 'name must be unique' 
-        })
-    }
-  
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
-})
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+  if (persons.map((p) => p.name).includes(body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
 
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
 
-const PORT = 3001
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
